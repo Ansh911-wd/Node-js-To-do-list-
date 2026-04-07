3d0559b584cf5543d83eb2274c67cdd748bbde41
Ansh kr singh <anshkrsingh321@gmail.com>
2026-04-07 19:21:05 +0000

    forms have been created

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import path from 'path'
+const app = express();
+const publicPath = path.resolve('public')
+app.use(express.static(publicPath))
+
+app.set("view engine",'ejs')
+
+app.get("/",(req,resp)=>{
+    resp.render("list")
+})
+app.get("/add",(req,resp)=>{
+    resp.render("add")
+})
+app.get("/update",(req,resp)=>{
+    resp.render("update")
+})
+app.post("/update",(req,resp)=>{
+    resp.redirect("/")
+})
+app.post("/add",(req,resp)=>{
+    resp.redirect("update")
+})
+app.listen(3200)
